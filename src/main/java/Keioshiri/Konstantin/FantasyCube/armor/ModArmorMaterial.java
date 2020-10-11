8ed62c3e879706a55f3632dc31828a8158d051d5
Keioshiri <72571493+Keioshiri@users.noreply.github.com>
2020-10-11 12:37:13 +0300

    Create ModArmorMaterial.java

---
+++ src/main/java/Keioshiri/Konstantin/FantasyCube/armor/ModArmorMaterial.java
@@ -0,0 +1,77 @@
+package Keioshiri.Konstantin.FantasyCube.armor;
+
+import Keioshiri.Konstantin.FantasyCube.FantasyCubeMod;
+import Keioshiri.Konstantin.FantasyCube.util.RegistryHandler;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+
+import java.util.function.Supplier;
+
+public enum ModArmorMaterial implements IArmorMaterial {
+
+    TITAN(FantasyCubeMod.MOD_ID + ":titan", 45, new int[] { 4, 7, 9, 4 }, 20,
+            SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 2.0F, () -> { return Ingredient.fromItems(RegistryHandler.TITAN.get());  });
+
+    private static final int[] MAX_DAMAGE_ARRAY = new int[] { 11, 16, 15, 13 };
+    private final String name;
+    private final int maxDamageFactor;
+    private final int[] damageReductionAmountArray;
+    private final int enchantability;
+    private final SoundEvent soundEvent;
+    private final float toughness;
+    private final Supplier<Ingredient> repairMaterial;
+
+    ModArmorMaterial(String name, int maxDamageFactor, int[] damageReductionAmountArray, int enchantability,
+                     SoundEvent soundEvent, float toughness, Supplier<Ingredient> repairMaterial){
+        this.name = name;
+        this.maxDamageFactor = maxDamageFactor;
+        this.damageReductionAmountArray = damageReductionAmountArray;
+        this.enchantability = enchantability;
+        this.soundEvent = soundEvent;
+        this.toughness = toughness;
+        this.repairMaterial = repairMaterial;
+    }
+
+    @Override
+    public int getDurability(EquipmentSlotType slotIn) {
+        return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+    }
+
+    @Override
+    public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+        return this.damageReductionAmountArray[slotIn.getIndex()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public SoundEvent getSoundEvent() {
+        return this.soundEvent;
+    }
+
+    @Override
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.get();
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public float getToughness() {
+        return this.toughness;
+    }
+
+    @Override
+    public float func_230304_f_() {
+        return 0;
+    }
+}
